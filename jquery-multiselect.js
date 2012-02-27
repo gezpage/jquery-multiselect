3550fe547c61be225c3f99b8c6567d257e9627d8
Gez Page <gez@ppgez.(none)>
2012-02-27 15:56:54 +0000

    Replaced live bindings with the newer 'on' function and events. Now using divs instead of labels for styling purposes. CSS file committed. Various other changes.

--- jquery-multiselect.js
+++ jquery-multiselect.js
@@ -5,21 +5,6 @@
 (function($){
         $.fn.extend({
                 multiselect: function() {
-                        // Hover binding
-                        $('.multiselect div').live({
-                                mouseover: function() {
-                                        $(this).children('label.sub').show();
-                                },
-                                mouseout: function() {
-                                        if (!$(this).children('label.main').hasClass('on')) {
-                                                $(this).children('label.sub').hide();
-                                        }
-                                }
-                        });
-                        // Click binding
-                        $('.multiselect div label').live('click', function() {
-                                change_state($(this));
-                        });
                         return this.each(function() {
                                 $div = $(this);
                                 // Apply class
@@ -27,56 +12,78 @@
                                 // Replace checkboxes with hidded input
                                 // And add classes for easy traversing
                                 $div.find('div').each(function() {
+                                        $(this).addClass('multiselect_container');
                                         var display = ($(this).find('input:first').is(':checked')) ? true : false;
                                         // Add 'main' class to first element which will be the heading
                                         $(this).find('label:first').addClass('main');
                                         $(this).find('input').each(function() {
                                                 var name = $(this).attr('name');
                                                 var $label = $('label[for="' + name + '"]');
+                                                var label = $label.text();
+                                                var $div = $('<div>').text(label).attr('for', name);
                                                 if (!$label.hasClass('main')) {
+                                                        $div.addClass('sub');
                                                         if (display) {
-                                                                $label.show();
+                                                                $div.show();
                                                         }
                                                         else {
-                                                                $label.hide();
+                                                                $div.hide();
                                                         }
-                                                        $label.addClass('sub');
+                                                }
+                                                else {
+                                                        $div.addClass('main');
                                                 }
                                                 var value = $(this).is(':checked') ? 'on' : 'off';
-                                                var new_input = '<input class="selector" type="hidden" name="' + name + '" id="' + name + '" value="' + value + '" /><br/>';
-                                                $label.addClass(value);
-                                                $(this).replaceWith(new_input);
+                                                var $input = $('<input type="hidden" name="' + name + '" id="' + name + '" value="' + value + '" />');
+                                                $div.addClass(value);
+                                                $div.on({
+                                                        click: function() {
+                                                                change_state($(this));
+                                                        }
+                                                });
+                                                $label.replaceWith($div);
+                                                $(this).replaceWith($input);
+                                        });
+                                        $(this).on({
+                                                mouseover: function() {
+                                                        $(this).children('div.sub').show();
+                                                },
+                                                mouseout: function() {
+                                                        if (!$(this).children('div.main').hasClass('on')) {
+                                                                $(this).children('div.sub').hide();
+                                                        }
+                                                }
                                         });
                                 });
                         });
                 }
         });
-        function change_state($label) {
-                if ($label.hasClass('on')) {
-                        switch_off($label);
-                        if ($label.hasClass('main')) {
-                                $label.siblings('label.sub').each(function() {
+        function change_state($div) {
+                if ($div.hasClass('on')) {
+                        switch_off($div);
+                        if ($div.hasClass('main')) {
+                                $div.siblings('div.sub').each(function() {
                                         switch_off($(this));
                                 });
                         }
                 }
                 else {
-                        switch_on($label);
-                        if ($label.hasClass('sub')) {
-                                $label.siblings('label.main').each(function() {
+                        switch_on($div);
+                        if ($div.hasClass('sub')) {
+                                $div.siblings('div.main').each(function() {
                                         switch_on($(this));
                                 });
                         }
                 }
         }
-        function switch_on($label) {
-                $input = $('input[name="' + $label.attr('for') + '"]');
-                $label.removeClass('off').addClass('on');
+        function switch_on($div) {
+                $input = $('input[name="' + $div.attr('for') + '"]');
+                $div.removeClass('off').addClass('on');
                 $input.val('on');
         }
-        function switch_off($label) {
-                $input = $('input[name="' + $label.attr('for') + '"]');
-                $label.removeClass('on').addClass('off');
+        function switch_off($div) {
+                $input = $('input[name="' + $div.attr('for') + '"]');
+                $div.removeClass('on').addClass('off');
                 $input.val('off');
         }
 })(jQuery);
